82093e40780d313d4eda61ffd883049d0c59b0f9
GAMIS65 <ivicko05@gmail.com>
2020-01-16 20:07:12 +0100

    cool

---
+++ Hnizak.py
@@ -0,0 +1,35 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "h?")
+
+@client.event
+async def on_ready():
+    print("I liek pie===")
+
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server.")
+
+@client.event
+async def on_member_remove(member, ctx):
+    print(f"{member} has left the server.")
+    await ctx.send(":WeirdChamp:")
+
+@client.command(brief='Latency of the bot', description='Latency of the bot')
+async def latency(ctx):
+    await ctx.send(f"{round(client.latency * 1000)}ms")
+
+@client.command()
+async def ask(ctx):
+    responses = ["Yes",
+                 "No",]
+    await ctx.send(f"{random.choice(responses)}")
+
+@client.command(brief='Delete messages', description='Delete messages')
+async def clear(ctx, amount = 5):
+    await ctx.channel.purge(limit=amount)
+
+
+client.run("NjY3MzgzNDI4NzY0Nzk0ODgx.XiB7lA.OODcVVaJ85TEpuN2BsKDfXci5L0")
---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python Hnizak.py
