88c1bfcc25c7226b8a5ebd8b2c82fc851ea38ca3
GAMIS65 <ivicko05@gmail.com>
2020-03-18 10:50:36 +0100

    no

--- cogs/steam.py
+++ cogs/steam.py
@@ -11,7 +11,7 @@ class steam(commands.Cog):
 
     @commands.command()
     async def steam(self, ctx, args):
-        URL = f"https://steamcommunity.com/profiles/{args}"
+        URL = f"https://steamcommunity.com/id/{args}"
         headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
         page = requests.get(URL, headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
