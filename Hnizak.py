a2f7a032c444d5a2bb0fd01bd492f827d3b5a93d
GAMIS65 <ivicko05@gmail.com>
2020-04-23 22:03:47 +0200

    removed token

--- Hnizak.py
+++ Hnizak.py
@@ -2,8 +2,15 @@
 import discord
 import datetime
 import os
+import dotenv
 from discord.ext import commands, tasks
 from itertools import cycle
+from dotenv import load_dotenv
+
+
+# Token
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Command prefix
 client = commands.Bot(command_prefix="h?")
@@ -41,6 +48,7 @@ async def on_ready():
     print(text1)
 
 
+# Change status every 30 seconds
 @tasks.loop(seconds=30)
 async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
@@ -50,4 +58,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run("NjY3MzgzNDI4NzY0Nzk0ODgx.XiHZOQ.as66DriXzoSArSprykiKhI65bBA")
+client.run(TOKEN)
